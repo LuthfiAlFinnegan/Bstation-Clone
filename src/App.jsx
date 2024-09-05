72c03d207975eda247eda954445d09fb25f9f967
luthfialfinnegan <gatesluthfi@gmail.com>
2024-09-05 19:19:28 +0700

    added basename for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import { LandingPage } from "./pages/landingPage";
 import { NotFoundPage } from "./pages/notFoundPage";
 
 function App() {
-  const router = createBrowserRouter([
-    { path: "/", element: <LandingPage /> },
-    { path: "*", element: <NotFoundPage /> },
-  ]);
+  const router = createBrowserRouter(
+    [
+      { path: "/", element: <LandingPage /> },
+      { path: "*", element: <NotFoundPage /> },
+    ],
+    { basename: "/Bstation-Clone" },
+  );
   return (
     <>
       <RouterProvider router={router} />
